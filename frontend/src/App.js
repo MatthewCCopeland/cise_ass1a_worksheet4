7d641b536b8bc4b48c1363bd31752825057478d3
Matthew Copeland <pry2051@aut.ac.nz>
2022-04-06 10:35:30 +1200

    Fixed broken lab instructions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
-// import Home from "./pages/Home";
-// import SEPractice from "./pages/SE-Practice";
-// import SubmitArticle from "./pages/Submit-Article";
-// import NotFoundPage from "./pages/404";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article";
+import {
+  Route,
+  Routes,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
 
 const App = () => {
   return (
@@ -11,11 +15,17 @@ const App = () => {
       <div>
         <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
         <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/SEPractice">Select the Practice</a></li>
-          <li><a href="/SubmitArticle">Submit an Article</a></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
         </ul>
         <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+          </Routes>
+
         </div>
       </div>
     </BrowserRouter>
