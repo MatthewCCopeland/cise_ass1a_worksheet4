ff1bac219df3f8a0a19811c49b294a630c477a3c
Matthew Copeland <pry2051@aut.ac.nz>
2022-04-06 14:00:50 +1200

    fixed the broken ass instructions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
+import NotFoundPage from "./pages/404";
 import {
   Route,
   Routes,
   NavLink,
-  BrowserRouter
+  BrowserRouter,
 } from "react-router-dom";
 
 const App = () => {
@@ -15,17 +16,17 @@ const App = () => {
       <div>
         <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
         <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink exact='true' to="/">Home</NavLink></li>
           <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
           <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
         </ul>
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact='true' path="/" element={<Home />} />
             <Route path="/SEPractice" element={<SEPractice />} />
             <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path='*' element={<NotFoundPage />} />
           </Routes>
-
         </div>
       </div>
     </BrowserRouter>
